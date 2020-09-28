d96c7dc44f4cc61b18fa13879ff9bd04c1591321
shakayami <34914547+shakayami@users.noreply.github.com>
2020-09-28 20:43:24 +0900

    Create fenwicktree.py

---
+++ fenwicktree.py
@@ -0,0 +1,21 @@
+class fenwick_tree():
+    n=1
+    data=[0 for i in range(n)]
+    def __init__(self,N):
+        self.n=N
+        self.data=[0 for i in range(N)]
+    def add(self,p,x):
+        assert 0<=p<self.n,"0<=p<n,p={0},n={1}".format(p,self.n)
+        p+=1
+        while(p<=self.n):
+            self.data[p-1]+=x
+            p+=p& -p
+    def sum(self,l,r):
+        assert (0<=l and l<=r and r<=self.n),"0<=l<=r<=n,l={0},r={1},n={2}".format(l,r,self.n)
+        return self.sum0(r)-self.sum0(l)
+    def sum0(self,r):
+        s=0
+        while(r>0):
+            s+=self.data[r-1]
+            r-=r&-r
+        return s
