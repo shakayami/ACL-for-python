50893fe47fe5ed41aa779376bb4004155f52ad14
shakayami <34914547+shakayami@users.noreply.github.com>
2022-02-22 18:49:03 +0900

    Update scc.py
    非再帰バージョンに変更
    
    参照　：　https://github.com/shakayami/ACL-for-python/issues/2

--- scc.py
+++ scc.py
@@ -1,5 +1,3 @@
-import sys
-sys.setrecursionlimit(10**6)
 def scc(N,edges):
     M=len(edges)
     start=[0]*(N+1)
@@ -12,31 +10,41 @@ def scc(N,edges):
     for e in edges:
         elist[counter[e[0]]]=e[1]
         counter[e[0]]+=1
-    NG=[0,0]
     visited=[]
     low=[0]*N
     Ord=[-1]*N
     ids=[0]*N
+    NG=[0,0]
     def dfs(v):
-        low[v]=NG[0]
-        Ord[v]=NG[0]
-        NG[0]+=1
-        visited.append(v)
-        for i in range(start[v],start[v+1]):
-            to=elist[i]
-            if Ord[to]==-1:
-                dfs(to)
-                low[v]=min(low[v],low[to])
+        stack=[(v,-1,0),(v,-1,1)]
+        while stack:
+            v,bef,t=stack.pop()
+            if t:
+                if bef!=-1 and Ord[v]!=-1:
+                    low[bef]=min(low[bef],Ord[v])
+                    stack.pop()
+                    continue
+                low[v]=NG[0]
+                Ord[v]=NG[0]
+                NG[0]+=1
+                visited.append(v)
+                for i in range(start[v],start[v+1]):
+                    to=elist[i]
+                    if Ord[to]==-1:
+                        stack.append((to,v,0))
+                        stack.append((to,v,1))
+                    else:
+                        low[v]=min(low[v],Ord[to])
             else:
-                low[v]=min(low[v],Ord[to])
-        if low[v]==Ord[v]:
-            while(True):
-                u=visited.pop()
-                Ord[u]=N
-                ids[u]=NG[1]
-                if u==v:
-                    break
-            NG[1]+=1
+                if low[v]==Ord[v]:
+                    while(True):
+                        u=visited.pop()
+                        Ord[u]=N
+                        ids[u]=NG[1]
+                        if u==v:
+                            break
+                    NG[1]+=1
+                low[bef]=min(low[bef],low[v])
     for i in range(N):
         if Ord[i]==-1:
             dfs(i)
