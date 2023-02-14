e6b6243cf4d45f2380bced4c5db0b0ac67e0d546
shakayami <34914547+shakayami@users.noreply.github.com>
2023-02-14 15:41:42 +0900

    Update convolution.py

--- convolution.py
+++ convolution.py
@@ -37,92 +37,126 @@ class FFT():
             res+=1
             x//=2
         return res
-    butterfly_first=True
-    butterfly_inv_first=True
-    sum_e=[0]*30
-    sum_ie=[0]*30
+    rank2=0
+    root=[]
+    iroot=[]
+    rate2=[]
+    irate2=[]
+    rate3=[]
+    irate3=[]
+    
     def __init__(self,MOD):
         self.mod=MOD
         self.g=self.primitive_root_constexpr(self.mod)
+        self.rank2=self.bsf(self.mod-1)
+        self.root=[0 for i in range(self.rank2+1)]
+        self.iroot=[0 for i in range(self.rank2+1)]
+        self.rate2=[0 for i in range(self.rank2)]
+        self.irate2=[0 for i in range(self.rank2)]
+        self.rate3=[0 for i in range(self.rank2-1)]
+        self.irate3=[0 for i in range(self.rank2-1)]
+        self.root[self.rank2]=pow(self.g,(self.mod-1)>>self.rank2,self.mod)
+        self.iroot[self.rank2]=pow(self.root[self.rank2],self.mod-2,self.mod)
+        for i in range(self.rank2-1,-1,-1):
+            self.root[i]=(self.root[i+1]**2)%self.mod
+            self.iroot[i]=(self.iroot[i+1]**2)%self.mod
+        prod=1;iprod=1
+        for i in range(self.rank2-1):
+            self.rate2[i]=(self.root[i+2]*prod)%self.mod
+            self.irate2[i]=(self.iroot[i+2]*iprod)%self.mod
+            prod=(prod*self.iroot[i+2])%self.mod
+            iprod=(iprod*self.root[i+2])%self.mod
+        prod=1;iprod=1
+        for i in range(self.rank2-2):
+            self.rate3[i]=(self.root[i+3]*prod)%self.mod
+            self.irate3[i]=(self.iroot[i+3]*iprod)%self.mod
+            prod=(prod*self.iroot[i+3])%self.mod
+            iprod=(iprod*self.root[i+3])%self.mod
     def butterfly(self,a):
         n=len(a)
         h=(n-1).bit_length()
-        if self.butterfly_first:
-            self.butterfly_first=False
-            es=[0]*30
-            ies=[0]*30
-            cnt2=self.bsf(self.mod-1)
-            e=pow(self.g,(self.mod-1)>>cnt2,self.mod)
-            ie=pow(e,self.mod-2,self.mod)
-            for i in range(cnt2,1,-1):
-                es[i-2]=e
-                ies[i-2]=ie
-                e=(e*e)%self.mod
-                ie=(ie*ie)%self.mod
-            now=1
-            for i in range(cnt2-2):
-                self.sum_e[i]=((es[i]*now)%self.mod)
-                now*=ies[i]
-                now%=self.mod
-        for ph in range(1,h+1):
-            w=1<<(ph-1)
-            p=1<<(h-ph)
-            now=1
-            for s in range(w):
-                offset=s<<(h-ph+1)
-                for i in range(p):
-                    l=a[i+offset]
-                    r=a[i+offset+p]*now
-                    r%=self.mod
-                    a[i+offset]=l+r
-                    a[i+offset]%=self.mod
-                    a[i+offset+p]=l-r
-                    a[i+offset+p]%=self.mod
-                now*=self.sum_e[(~s & -~s).bit_length()-1]
-                now%=self.mod
+        
+        LEN=0
+        while(LEN<h):
+            if (h-LEN==1):
+                p=1<<(h-LEN-1)
+                rot=1
+                for s in range(1<<LEN):
+                    offset=s<<(h-LEN)
+                    for i in range(p):
+                        l=a[i+offset]
+                        r=a[i+offset+p]*rot
+                        a[i+offset]=(l+r)%self.mod
+                        a[i+offset+p]=(l-r)%self.mod
+                    rot*=self.rate2[(~s & -~s).bit_length()-1]
+                    rot%=self.mod
+                LEN+=1
+            else:
+                p=1<<(h-LEN-2)
+                rot=1
+                imag=self.root[2]
+                for s in range(1<<LEN):
+                    rot2=(rot*rot)%self.mod
+                    rot3=(rot2*rot)%self.mod
+                    offset=s<<(h-LEN)
+                    for i in range(p):
+                        a0=a[i+offset]
+                        a1=a[i+offset+p]*rot
+                        a2=a[i+offset+2*p]*rot2
+                        a3=a[i+offset+3*p]*rot3
+                        a1na3imag=(a1-a3)%self.mod*imag
+                        a[i+offset]=(a0+a2+a1+a3)%self.mod
+                        a[i+offset+p]=(a0+a2-a1-a3)%self.mod
+                        a[i+offset+2*p]=(a0-a2+a1na3imag)%self.mod
+                        a[i+offset+3*p]=(a0-a2-a1na3imag)%self.mod
+                    rot*=self.rate3[(~s & -~s).bit_length()-1]
+                    rot%=self.mod
+                LEN+=2
+                
     def butterfly_inv(self,a):
         n=len(a)
         h=(n-1).bit_length()
-        if self.butterfly_inv_first:
-            self.butterfly_inv_first=False
-            es=[0]*30
-            ies=[0]*30
-            cnt2=self.bsf(self.mod-1)
-            e=pow(self.g,(self.mod-1)>>cnt2,self.mod)
-            ie=pow(e,self.mod-2,self.mod)
-            for i in range(cnt2,1,-1):
-                es[i-2]=e
-                ies[i-2]=ie
-                e=(e*e)%self.mod
-                ie=(ie*ie)%self.mod
-            now=1
-            for i in range(cnt2-2):
-                self.sum_ie[i]=((ies[i]*now)%self.mod)
-                now*=es[i]
-                now%=self.mod
-        for ph in range(h,0,-1):
-            w=1<<(ph-1)
-            p=1<<(h-ph)
-            inow=1
-            for s in range(w):
-                offset=s<<(h-ph+1)
-                for i in range(p):
-                    l=a[i+offset]
-                    r=a[i+offset+p]
-                    a[i+offset]=l+r
-                    a[i+offset]%=self.mod
-                    a[i+offset+p]=(l-r)*inow
-                    a[i+offset+p]%=self.mod
-                inow*=self.sum_ie[(~s & -~s).bit_length()-1]
-                inow%=self.mod
+        LEN=h
+        while(LEN):
+            if (LEN==1):
+                p=1<<(h-LEN)
+                irot=1
+                for s in range(1<<(LEN-1)):
+                    offset=s<<(h-LEN+1)
+                    for i in range(p):
+                        l=a[i+offset]
+                        r=a[i+offset+p]
+                        a[i+offset]=(l+r)%self.mod
+                        a[i+offset+p]=(l-r)*irot%self.mod
+                    irot*=self.irate2[(~s & -~s).bit_length()-1]
+                    irot%=self.mod
+                LEN-=1
+            else:
+                p=1<<(h-LEN)
+                irot=1
+                iimag=self.iroot[2]
+                for s in range(1<<(LEN-2)):
+                    irot2=(irot*irot)%self.mod
+                    irot3=(irot*irot2)%self.mod
+                    offset=s<<(h-LEN+2)
+                    for i in range(p):
+                        a0=a[i+offset]
+                        a1=a[i+offset+p]
+                        a2=a[i+offset+2*p]
+                        a3=a[i+offset+3*p]
+                        a2na3iimag=(a2-a3)*iimag%self.mod
+                        a[i+offset]=(a0+a1+a2+a3)%self.mod
+                        a[i+offset+p]=(a0-a1+a2na3iimag)*irot%self.mod
+                        a[i+offset+2*p]=(a0+a1-a2-a3)*irot2%self.mod
+                        a[i+offset+3*p]=(a0-a1-a2na3iimag)*irot3%self.mod
+                    irot*=self.irate3[(~s & -~s).bit_length()-1]
+                    irot%=self.mod
+                LEN-=2
     def convolution(self,a,b):
         n=len(a);m=len(b)
         if not(a) or not(b):
             return []
         if min(n,m)<=40:
-            if n<m:
-                n,m=m,n
-                a,b=b,a
             res=[0]*(n+m-1)
             for i in range(n):
                 for j in range(m):
@@ -134,9 +168,7 @@ class FFT():
         b=b+[0]*(z-m)
         self.butterfly(a)
         self.butterfly(b)
-        c=[0]*z
-        for i in range(z):
-            c[i]=(a[i]*b[i])%self.mod
+        c=[(a[i]*b[i])%self.mod for i in range(z)]
         self.butterfly_inv(c)
         iz=pow(z,self.mod-2,self.mod)
         for i in range(n+m-1):
