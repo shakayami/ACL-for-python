835174db38cf34cf79c993845af8848e1aa85894
shakayami <34914547+shakayami@users.noreply.github.com>
2023-02-15 18:57:00 +0900

    Update maxflow.py
    辺の管理をdictからlistにすることで少し速くなった

--- maxflow.py
+++ maxflow.py
@@ -12,79 +12,82 @@ class mf_graph:
         assert 0<=To and To<self.n
         assert 0<=cap
         m=len(self.pos)
-        self.pos.append((From,len(self.g[From])))
         from_id=len(self.g[From])
+        self.pos.append([From,from_id])
         to_id=len(self.g[To])
         if From==To:to_id+=1
-        self.g[From].append({"to":To,"rev":to_id,"cap":cap})
-        self.g[To].append({"to":From,"rev":from_id,"cap":0})
+        self.g[From].append([To,to_id,cap])
+        self.g[To].append([From,from_id,0])
         return m
     def get_edge(self,i):
         m=len(self.pos)
         assert 0<=i and i<m
         _e=self.g[self.pos[i][0]][self.pos[i][1]]
-        _re=self.g[_e["to"]][_e["rev"]]
-        return {"from":self.pos[i][0],
-                "to":_e["to"],
-                "cap":_e["cap"]+_re["cap"],
-                "flow":_re["cap"]}
+        _re=self.g[_e[0]][_e[1]]
+        return [self.pos[i][0],_e[0],_e[2]+_re[2],_re[2]]
     def edges(self):
         m=len(self.pos)
         result=[]
         for i in range(m):
             result.append(self.get_edge(i))
         return result
+    def edges_debug(self):
+        m=len(self.pos)
+        result=[]
+        for i in range(m):
+            a,b,c,d=self.get_edge(i)
+            result.append({"from":a,"to":b,"cap":c,"flow":d})
+        return result
+
     def change_edge(self,i,new_cap,new_flow):
         m=len(self.pos)
         assert 0<=i and i<m
         assert 0<=new_flow and new_flow<=new_cap
         _e=self.g[self.pos[i][0]][self.pos[i][1]]
-        _re=self.g[_e["to"]][_e["rev"]]
-        _e["cap"]=new_cap-new_flow
-        _re["cap"]=new_flow
+        _re=self.g[_e[0]][_e[1]]
+        _e[2]=new_cap-new_flow
+        _re[2]=new_flow
     def flow(self,s,t,flow_limit=(1<<63)-1):
         assert 0<=s and s<self.n
         assert 0<=t and t<self.n
         assert s!=t
-        level=[0 for i in range(self.n)]
-        Iter=[0 for i in range(self.n)]
-        que=deque([])
         def bfs():
-            for i in range(self.n):level[i]=-1
+            level=[-1 for i in range(self.n)]
             level[s]=0
             que=deque([])
             que.append(s)
             while(que):
                 v=que.popleft()
-                for e in self.g[v]:
-                    if e["cap"]==0 or level[e["to"]]>=0:continue
-                    level[e["to"]]=level[v]+1
-                    if e["to"]==t:return
-                    que.append(e["to"])
-        def dfs(func,v,up):
+                for to,rev,cap in self.g[v]:
+                    if cap==0 or level[to]>=0:continue
+                    level[to]=level[v]+1
+                    if to==t:return level
+                    que.append(to)
+            return level
+        def dfs(v,up):
             if (v==s):return up
             res=0
             level_v=level[v]
             for i in range(Iter[v],len(self.g[v])):
-                e=self.g[v][i]
-                if (level_v<=level[e["to"]] or self.g[e["to"]][e["rev"]]["cap"]==0):continue
-                d=func(func,e["to"],min(up-res,self.g[e["to"]][e["rev"]]["cap"]))
+                to,rev,cap=self.g[v][i]
+                if (level_v<=level[to] or self.g[to][rev][2]==0):continue
+                d=dfs(to,min(up-res,self.g[to][rev][2]))
                 if d<=0:continue
-                self.g[v][i]["cap"]+=d
-                self.g[e["to"]][e["rev"]]["cap"]-=d
+                self.g[v][i][2]+=d
+                self.g[to][rev][2]-=d
                 res+=d
                 if res==up:return res
             level[v]=self.n
             return res
+
         flow=0
         while(flow<flow_limit):
-            bfs()
+            level=bfs()
             if level[t]==-1:break
-            for i in range(self.n):Iter[i]=0
-            while(flow<flow_limit):
-                f=dfs(dfs,t,flow_limit-flow)
-                if not(f):break
-                flow+=f
+            Iter=[0 for i in range(self.n)]
+            f=dfs(t,flow_limit-flow)
+            if not(f):break
+            flow+=f
         return flow
     def min_cut(self,s):
         visited=[False for i in range(self.n)]
@@ -93,8 +96,8 @@ class mf_graph:
         while(len(que)>0):
             p=que.popleft()
             visited[p]=True
-            for e in self.g[p]:
-                if e["cap"] and not(visited[e["to"]]):
-                    visited[e["to"]]=True
-                    que.append(e["to"])
+            for to,rev,cap in self.g[p]:
+                if cap and not(visited[to]):
+                    visited[to]=True
+                    que.append(to)
         return visited
